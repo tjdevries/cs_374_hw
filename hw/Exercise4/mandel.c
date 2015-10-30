5bf0da84faa86077dcc4c48c3c3c5cbca18f52aa
TJ DeVries <devries.timothyj@gmail.com>
2015-10-29 22:17:55 -0400

    A working copy of the part 1 of the project

--- hw/Exercise4/mandel.c
+++ hw/Exercise4/mandel.c
@@ -7,7 +7,7 @@
 #include <math.h>
 #include <sys/time.h>
 #include <mpi/mpi.h>
-// #include <mpi/mpe.h>
+#include <mpi/mpe.h>
 #include "display.h"
 
 /* compute the mandelbrot-set function for a given
@@ -18,8 +18,8 @@
  * POST: ans_x and ans_y contain the results of the mandelbrot-set
  *        function for x, y, and c.
  */
-void compute(short x, short y, short c_real, short c_imag,
-        short *ans_x, short *ans_y)
+void compute(double x, double y, double c_real, double c_imag,
+        double *ans_x, double *ans_y)
 {
         *ans_x = x*x - y*y + c_real;
         *ans_y = 2*x*y + c_imag;
@@ -29,7 +29,7 @@ void compute(short x, short y, short c_real, short c_imag,
  * Receive: doubles x and y.
  * Return: x^2 + y^2.
  */
-short distance(short x, short y)
+double distance(double x, double y)
 {
         return(x*x + y*y);
 }
@@ -48,51 +48,37 @@ int main(int argc, char* argv[])
                id = 0,
                numProc,
                chunk = 0;
-    short      spacing=.005,
+    double     spacing=.005,
                x,
                y,
                c_real,
                c_imag,
                x_center = 1.0,
-               y_center = 0.0;
+               y_center = 0.0,
+               starttime;
 
     // Initialize our MPI items
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numProc);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
-
-    // Optional input items
-    /*
-       printf("\nEnter spacing (.005): "); fflush(stdout);
-       scanf("%lf",&spaci);
-       printf("\nEnter coordinates of center point (0,0): "); fflush(stdout);
-       scanf("%lf %lf", &x_center, &y_center);
-       printf("\nSpacing=%lf, center=(%lf,%lf)\n",
-       spacing, x_center, y_center);
-       */
-
+    if(id == 0){starttime = MPI_Wtime();}
     // Set the size of our chunks.
     chunk = WINDOW_SIZE / numProc;
 
+    // Do some debug printing
     if (verbose) { printf("Chunk size set to %d. Total size is %d", chunk, chunk * numProc); }
 
     // Each process creates its own local chunkarray
-    short chunkarray[(chunk * WINDOW_SIZE)+1024];
-    int end;
+    short chunkarray[(chunk * WINDOW_SIZE)];
+    int end = (id +1 ) * chunk;
 
     // Loop through and calculate the MPI items
     int start = id * chunk;
     // Make sure that we get all the values, instead of missing one in case of
     //  none perfect division.
-    if (id != numProc - 1) {
-        end = (id + 1) * chunk;
-    }
-    else {
-        end = (id+1) * chunk; //WINDOW_SIZE; //to see if this is causing the seg fault
-    }
+    if (verbose) { printf("Process %d has start: %d, end: %d\n", id, start, end); }
 
-    if (verbose) { printf("Process %d has start: %d, end: %d", id, start, end); }
-
+    // Loop through and calculate the values.
     for (ix = start; ix < end; ix++)
     {
         for (iy = 0; iy <  WINDOW_SIZE; iy++)
@@ -106,6 +92,7 @@ int main(int argc, char* argv[])
                  compute(x,y,c_real,c_imag,&x,&y);
                  n++;
             }
+
             if (n < 50) {
             // 0 for red
             //  Because these are the local arrays, they should be 0 relative.
@@ -126,36 +113,35 @@ int main(int argc, char* argv[])
 
     // MPI_Gather(const void *sendbuf, int sendcount, MPI_Datatype sendtype,
     //  void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm)
-    // Not sure if this will work with the chunk size every time because of the way we set chunks for the last option.
+
     MPI_Gather(&chunkarray, chunk*WINDOW_SIZE, MPI_SHORT, &array, chunk*WINDOW_SIZE, MPI_SHORT, 0, MPI_COMM_WORLD);
 
     if (id == 0) {
+        double endtime = MPI_Wtime();
+        double time = endtime - starttime;
+        printf("It took %fl seconds\n", time);
         // Initialize the graph. Only done on the main id process
-        //MPE_XGraph graph;
-        //MPE_Open_graphics( &graph, MPI_COMM_WORLD, getDisplay(), -1, -1, WINDOW_SIZE, WINDOW_SIZE, 0);
+        MPE_XGraph graph;
+        MPE_Open_graphics( &graph, MPI_COMM_WORLD, getDisplay(), -1, -1, WINDOW_SIZE, WINDOW_SIZE, 0);
 
         // Begin to print out the graph
-        if (verbose) { printf("\n"); } // Print out a newline to separate the array from previous prints
         for(ix = 0; ix < WINDOW_SIZE; ix++) {
-            if (verbose) { printf("\n"); } // Print out a new line after each row of the matrix
-
             // Cycle through the matrix rows.
             for(iy = 0; iy < WINDOW_SIZE; iy++) {
                 // Print out an array
-                if (verbose) { printf("%i",array[(ix*1024)+iy]); }
-
-                if(array[ix * iy] == 0){
-                    //MPE_Draw_point(graph, ix, iy, MPE_RED);
+                if(array[iy + ix * WINDOW_SIZE] == 0){
+                    MPE_Draw_point(graph, ix, iy, MPE_RED);
                 }
                 else{
-                    //MPE_Draw_point(graph, ix, iy, MPE_BLACK);
+                    MPE_Draw_point(graph, ix, iy, MPE_BLACK);
                 }
             }
         }
         printf("\nClick in the window to continue...\n");
-        //MPE_Get_mouse_press(graph, &ix, &iy, &button );
-        //MPE_Close_graphics( &graph );
+        MPE_Get_mouse_press(graph, &ix, &iy, &button );
+        MPE_Close_graphics( &graph );
     }
     MPI_Finalize();
     return 0;
 }
+
